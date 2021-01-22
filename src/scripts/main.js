ad63fcbc80c3c5bfc284d3d7103fc59e75caae3a
Serhii Alieksieievych <kajot1863@gmail.com>
2021-01-22 21:24:50 +0200

    Slider was activated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,30 @@ toggler.onclick = function toggleSidebar() {
   nav.classList.toggle('header__nav--has-popup');
   toggler.classList.toggle('toggler--open');
 };
+
+const slider = document.querySelector('.slider');
+const goToPrevButton = slider.querySelector('.slider__button--left');
+const goToNextButton = slider.querySelector('.slider__button--right');
+
+const demo = slider.querySelector('.slider__demo');
+const demoIndexArr = [0, 1, 2];
+let actualDemoIndex = 0;
+
+const rotateClockwise = () => {
+  return actualDemoIndex + 1 <= demoIndexArr.length - 1
+    ? actualDemoIndex + 1 : 0;
+};
+const rotateCounterclockwise = () => {
+  return actualDemoIndex - 1 >= 0
+    ? actualDemoIndex - 1 : demoIndexArr.length - 1;
+};
+
+goToPrevButton.addEventListener('click', () => {
+  demo.className = `slider__demo slider__demo--${rotateCounterclockwise()}`;
+  actualDemoIndex = rotateCounterclockwise();
+});
+
+goToNextButton.addEventListener('click', () => {
+  demo.className = `slider__demo slider__demo--${rotateClockwise()}`;
+  actualDemoIndex = rotateClockwise();
+});
